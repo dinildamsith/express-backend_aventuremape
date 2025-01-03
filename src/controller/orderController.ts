3fc4644493d765bea079936ccd7d1d0033b1225c
dinildamsith <dinildamsith52@gmail.com>
2025-01-03 22:54:34 +0530

    vehicle have pending order get

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -20,7 +20,7 @@ class ResponseDto {
 
 const responseDTO = new ResponseDto();
 
-
+//--------------------------------- Create Order Buy Vehicle Rent ---------------------------------
 router.post('/api/v1/order/perches-vehicle', async (req, res) => {
     const { orderType,  orderPrice, orderStartDuration, orderEndDuration, buyerEmail, vehicle } = req.body;
 
@@ -50,5 +50,33 @@ router.post('/api/v1/order/perches-vehicle', async (req, res) => {
         return res.status(500).json(responseDTO);
     }
 });
+
+//-------------------- Pending Vehicle Orders Get ---------------------------------
+router.get('/api/v1/order/pending-vehicle-orders/:email', async (req, res) => {
+
+    const { email } = req.params;
+
+    try {
+        const orders = await OrderModel.find({ vehicle: email,orderStatus: 'PENDING', orderType: 'RENT_VEHICLE' });
+
+        if (orders.length != 0) {
+            responseDTO.status = 'SUCCESS';
+            responseDTO.description = 'Pending Vehicle Orders Fetched Successfully';
+            responseDTO.data = orders;
+            return res.status(200).json(responseDTO);
+        } else {
+            responseDTO.status = 'FAILED';
+            responseDTO.description = 'No pending vehicle orders found';
+            return res.status(404).json(responseDTO);
+        }
+
+    } catch (error) {
+        console.error(error);
+        responseDTO.status = 'FAILED';
+        responseDTO.description = 'An error occurred while fetching pending vehicle orders';
+        return res.status(500).json(responseDTO);
+    }
+});
+
 
 export default router
