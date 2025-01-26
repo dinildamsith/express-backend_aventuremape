f67371bf51de40311da5ebf61d71707210b43266
dinildamsith <dinildamsith52@gmail.com>
2025-01-26 14:50:54 +0530

    change apply

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,14 @@ import GuideController from "./controller/guideController";
 import vehicleController from "./controller/vehicleController";
 import cors from 'cors';
 import imageUploadController from "./controller/imageUploadController";
-import orderController from './controller/orderController'
+import orderController from './controller/orderController';
 
 
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(cors());
-
-
+app.use(cors({ origin: true })); // Allow all origins
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://ddamsith17:9JnkaF5zwEAoMOmb@aventuremape.uetls.mongodb.net/?retryWrites=true&w=majority&appName=aventuremape')
@@ -26,10 +24,10 @@ mongoose.connect('mongodb+srv://ddamsith17:9JnkaF5zwEAoMOmb@aventuremape.uetls.m
     });
 
 // Use the routes for buyer-related actions
-app.use('/adventure-map', BuyerController,GuideController, vehicleController, imageUploadController, orderController);
+app.use('/adventure-map', BuyerController, GuideController, vehicleController, imageUploadController, orderController);
 
 // Start the server
 const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running and accessible on http://0.0.0.0:${PORT}`);
 });
